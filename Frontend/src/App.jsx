6b156981c442425a5afc02a41d0f6644a26ceb82
Gopal kumar <gopal851215@gmail.com>
2026-03-24 11:30:01 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+<>
+    <h1 className="text-3xl font-bold underline bg-blue-500">
+      Hello world!
+    </h1>
+</>
+  )
+}
+
+export default App
